20dada44b6b979c3a027317eeb1e32195c612c61
Dinesh Kottakota <95535995+dineshkottaKota@users.noreply.github.com>
2024-10-08 01:36:38 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
 
 
-  const url='http://localhost:4000'
+  const url='https://food-delivery-app-1-faqc.onrender.com'
   return (
     <div>
       <ToastContainer/>
