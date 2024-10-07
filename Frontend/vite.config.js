0f9e87bca0cf26109b7c07a9038eabb01b4e375e
Dinesh Kottakota <95535995+dineshkottaKota@users.noreply.github.com>
2024-10-08 00:43:52 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Bind to 0.0.0.0
+    port: 5173,       // You can specify a port if needed (Render automatically sets it)
+  },
+  plugins: [react()]
 })
